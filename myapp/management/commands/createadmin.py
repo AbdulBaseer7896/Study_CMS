96b669c03126fbf01cbb91825aef9782f087b07b
AbdulBaseer7896 <abdulbasirqazi7896@gmail.com>
2026-03-14 20:13:08 +0500

    v1.11) update the createadmin.py file

--- myapp/management/commands/createadmin.py
+++ myapp/management/commands/createadmin.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 import os
+from datetime import date
 
 User = get_user_model()
 
@@ -12,11 +13,15 @@ class Command(BaseCommand):
         email = os.getenv("DJANGO_ADMIN_EMAIL", "admin@gmail.com")
         password = os.getenv("DJANGO_ADMIN_PASSWORD", "pass1234")
 
+        # Default DOB to Jan 1, 2000
+        dob = date(2000, 1, 1)
+
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(
                 username=username,
                 email=email,
-                password=password
+                password=password,
+                dob=dob,  # Add this
             )
             self.stdout.write(self.style.SUCCESS("Admin user created"))
         else:
